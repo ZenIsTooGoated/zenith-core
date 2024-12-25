c365c140250e8887eeabc4220b0e31c616674669
ZenIsTooGoated <136510780+cuenhub@users.noreply.github.com>
2024-12-26 01:31:29 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -6,7 +6,7 @@ local gameScripts = {
 local scriptLoaded = false
 for gameName, placeId in pairs(gameScripts) do
     if game.PlaceId == placeId then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/cuenhub/zenith-core/refs/heads/main/games/"..gameName))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/cuenhub/zenith-core/refs/heads/main/games/"..gameName..".lua"))()
         scriptLoaded = true
         break
     end
