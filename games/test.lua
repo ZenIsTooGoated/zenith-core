fae780c471ec90ea0fa819121923ea517f48c240
ZenIsTooGoated <136510780+cuenhub@users.noreply.github.com>
2025-01-19 13:53:01 +0800

    Update test.lua

--- games/test.lua
+++ games/test.lua
@@ -1,1 +1,27 @@
 
+local httpService = game:GetService("HttpService")
+local scriptManager = {}
+do
+    scriptManager.Library = nil
+
+end
+
+function scriptManager:SetLibrary(library)
+    self.Library = library
+end
+
+function scriptManager:BuildSection(tab)
+    
+
+    assert(self.Library, "set the library first")
+    local library = self.Library
+    
+    local gameSection = tab:Section({
+        Side = "Left"
+    })
+
+    
+
+end
+
+return 
